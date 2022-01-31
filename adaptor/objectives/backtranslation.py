e9ea0ff5b532e87803bbdb471465a5ea77431b66
Nikola Groverova <nikola.groverova@gaussalgo.com>
2022-01-31 14:21:47 +0100

    caching for backtranslation fixed

--- adaptor/objectives/backtranslation.py
+++ adaptor/objectives/backtranslation.py
@@ -18,22 +18,45 @@ class BackTranslator(torch.nn.Module):
 
     def __init__(self, model_name_or_path: str, device: Optional[str] = None):
         super().__init__()
-        self.device = device if device is not None else "cuda" if torch.cuda.is_available() else "cpu"
+        self.device = (
+            device
+            if device is not None
+            else "cuda"
+            if torch.cuda.is_available()
+            else "cpu"
+        )
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
-        self.translator = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path).to(self.device)
-    @lru_cache(maxsize = 10000)
+        self.translator = AutoModelForSeq2SeqLM.from_pretrained(model_name_or_path).to(
+            self.device
+        )
+
+    @staticmethod
+    @lru_cache(maxsize=10000, typed=False)
+    def _translate_one(
+        text: str,
+        tokenizer: AutoTokenizer,
+        model: torch.nn.Module,
+    ) -> str:
+        inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+        output = (
+            model.generate(
+                input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]
+            )
+            .detach()
+            .cpu()
+        )
+        return tokenizer.decode(*output, skip_special_tokens=True)
+
     def translate(self, texts: List[str]) -> List[str]:
         """
         Translates input texts using the given translation model.
         :param texts: texts to be translated.
         :return: translated texts.
         """
-
-        inputs = self.tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
-        outputs = self.translator.generate(**inputs.to(self.device))
-        translations = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
-
+        translations = [
+            self._translate_one(text, self.tokenizer, self.translator) for text in texts
+        ]
         return translations
 
 
@@ -42,29 +65,41 @@ class BackTranslation(Sequence2SequenceMixin, UnsupervisedObjective):
     BackTranslation Objective can be used for unsupervised adaptation of translator to *Target* domain.
     """
 
-    def __init__(self,
-                 *args,
-                 back_translator: BackTranslator,
-                 **kwargs):
+    def __init__(self, *args, back_translator: BackTranslator, **kwargs):
         super().__init__(*args, **kwargs)
-        logger.warning("%s expects target-language texts as input_texts_or_path. This is not further checked. " % self)
+        logger.warning(
+            "%s expects target-language texts as input_texts_or_path. This is not further checked. "
+            % self
+        )
 
         self.translator = back_translator
-        self.collator = DataCollatorForSeq2Seq(self.tokenizer, self.compatible_head_model)
+        self.collator = DataCollatorForSeq2Seq(
+            self.tokenizer, self.compatible_head_model
+        )
 
     def _construct_batch(self, target_texts_batch: List[str]):
         translated_source_texts = self.translator.translate(target_texts_batch)
         features_batch = []
         for src_text, tgt_text in zip(translated_source_texts, target_texts_batch):
-            sample_features = self.tokenizer(src_text, truncation=True, padding="longest")
+            sample_features = self.tokenizer(
+                src_text, truncation=True, padding="longest"
+            )
             with self.tokenizer.as_target_tokenizer():
-                sample_targets = self.tokenizer(tgt_text, truncation=True, padding="longest")
-            features_batch.append({"input_ids": sample_features.input_ids,
-                                   "attention_mask": sample_features.attention_mask,
-                                   "labels": sample_targets.input_ids})
+                sample_targets = self.tokenizer(
+                    tgt_text, truncation=True, padding="longest"
+                )
+            features_batch.append(
+                {
+                    "input_ids": sample_features.input_ids,
+                    "attention_mask": sample_features.attention_mask,
+                    "labels": sample_targets.input_ids,
+                }
+            )
         return self.collator(features_batch)
 
-    def _get_seq2seq_collated_iterator(self, source_texts: Iterable[str], target_texts: Iterable[str]) -> Iterator:
+    def _get_seq2seq_collated_iterator(
+        self, source_texts: Iterable[str], target_texts: Iterable[str]
+    ) -> Iterator:
         """
         Constructs collated inputs from the target-side monolingual texts, using self.translator.
         :param source_texts: Unsupervised input texts -> Must match target texts.
@@ -75,8 +110,12 @@ class BackTranslation(Sequence2SequenceMixin, UnsupervisedObjective):
 
         for source_text, target_text in zip(source_texts, target_texts):
             if source_text != target_text:
-                logger.error("%s: source and target texts must match. Source: \n%s\nTarget:\n%s",
-                             self, source_text, target_text)
+                logger.error(
+                    "%s: source and target texts must match. Source: \n%s\nTarget:\n%s",
+                    self,
+                    source_text,
+                    target_text,
+                )
             targets_batch.append(target_text)
 
             if len(targets_batch) == self.batch_size:
@@ -89,4 +128,4 @@ class BackTranslation(Sequence2SequenceMixin, UnsupervisedObjective):
             yield self._construct_batch(targets_batch)
 
     def cache_info(self) -> _CacheInfo:
-        return self.translator.translate.cache_info()
+        return self.translator._translate_one.cache_info()
