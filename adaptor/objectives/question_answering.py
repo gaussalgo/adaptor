ed1bdc44cc8654aec4794432cec77101faed7f6b
Nikola Groverova <nikola.groverova@gaussalgo.com>
2022-07-15 17:27:21 +0200

    ExtractiveQA objective compute loss

--- adaptor/objectives/question_answering.py
+++ adaptor/objectives/question_answering.py
@@ -26,13 +26,13 @@ class ExtractiveQA(SupervisedObjective):
             for src_text, text_pair, label in zip(*self._per_split_iterators_text_pair(split)):
                 out_sample = self.tokenizer(src_text, text_pair=text_pair, max_length=MAX_LENGTH, truncation=True,padding='max_length')
                 tokenized_label = self.tokenizer(label, max_length=MAX_LENGTH, truncation=True,padding='max_length')
-
+                label_wo_padding = self.tokenizer(label)
                 #find indexes for answer:
                 if set(tokenized_label["input_ids"][1:-1]).issubset(set(out_sample["input_ids"])): 
                     start_position=out_sample["input_ids"].index(tokenized_label["input_ids"][1:-1][0])
-                    answer_length=len(tokenized_label["input_ids"][1:-1])
+                    answer_length=len(label_wo_padding["input_ids"][1:-1])
                 else:
-                    start_position=-1
+                    start_position=0
                     answer_length=0
                 end_position = start_position+answer_length
                 out_sample["label"] = tokenized_label["input_ids"]
@@ -41,7 +41,6 @@ class ExtractiveQA(SupervisedObjective):
 
                 batch_features.append(out_sample)
                 if len(batch_features) == self.batch_size:
-                    print(batch_features)
                     yield collator(batch_features)
                     batch_features = []
 
@@ -61,19 +60,17 @@ class ExtractiveQA(SupervisedObjective):
         :param attention_mask: Mask of the tokens to compute loss on.
         :return: loss value with grad_fn.
         """
-
-        loss_fct = torch.nn.CrossEntropyLoss(ignore_index=ignored_index)
+        loss_fct = torch.nn.CrossEntropyLoss()
         #split concatonated outputs for start and end:
-        start_logits, end_logits = torch.split(logit_outputs, dim = 0)
+        start_logits = logit_outputs["start_logits"]
+        end_logits = logit_outputs["end_logits"]
 
-        ignored_index = start_logits.size(1)
-        start_positions = start_positions.clamp(0, ignored_index)
-        end_positions = end_positions.clamp(0, ignored_index)
+        start_positions = inputs["start_position"]
+        end_positions = inputs["end_position"]
 
         start_loss = loss_fct(start_logits, start_positions)
         end_loss = loss_fct(end_logits, end_positions)
         total_loss = (start_loss + end_loss) / 2
-
         return total_loss
 
 
