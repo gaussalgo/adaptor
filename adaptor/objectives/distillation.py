5e7fabb6e4ca479a60d36a4d57ac7e1089419749
stefanik12 <stefanik.mich@gmail.com>
2022-09-15 13:44:31 +0200

    Distillation: cleanup & comments

--- adaptor/objectives/distillation.py
+++ adaptor/objectives/distillation.py
@@ -13,16 +13,35 @@ from adaptor.objectives.objective_base import Objective
 
 
 class Distillation(Objective, abc.ABC):
+    """
+    Model-agnostic implementation of distillation, as introduced in DistilBERT paper: https://arxiv.org/abs/1910.01108
+    This implementation will work out-of-box with default parameters for models with the same prediction dimensionality
+    (= number of categories, or vocab size for LMs).
+    Note that objectives can occasionally produce more than one output: currently only in ExtractiveQA;
+    in such cases, the model prediction can be flattened in a custom wrapper.
+    """
 
     def __init__(self, *args,
-                 distilled_model: PreTrainedModel,
+                 teacher_model: PreTrainedModel,
                  temperature: int = 1,
                  logits_ce_loss_weight: int = 1,
                  hidden_cossim_loss_weight: int = 1,
                  add_hidden_states_loss: bool = False,
                  restrict_loss_to_mask: bool = False,
                  **kwargs):
-        self.teacher_model = distilled_model
+        """
+        See the distillation parameters description in https://arxiv.org/abs/1910.01108.
+        :param teacher_model: A model to distill the prediction from.
+        :param temperature: Distillation intensity; The smaller, the stronger. See DistilBERT paper for details.
+        :param logits_ce_loss_weight: Relative weight of Logits' cross-entropy loss.
+        :param hidden_cossim_loss_weight: Relative weights of hidden states loss.
+        :param add_hidden_states_loss: Whether to also include hidden states loss.
+            Note that hidden states' loss will work only when distilling from the model
+            with same hidden states' dimensionality. Defaults to false.
+        :param restrict_loss_to_mask: Whether to compute selected losses only from the attended positions
+            (set to True), or from all positions (default, set to False)
+        """
+        self.teacher_model = teacher_model
 
         self.temperature = temperature
         self.logits_ce_loss_weight = logits_ce_loss_weight
@@ -48,10 +67,11 @@ class Distillation(Objective, abc.ABC):
                 objective_args_for_head_config["output_hidden_states"] = True
             else:
                 objective_args_for_head_config = {"output_hidden_states": True}
-        return super(Distillation, self).register_compatible_head_model(lang_module,
-                                                                        other_objective,
-                                                                        objective_args_for_head_config,
-                                                                        preloaded_module)
+
+        return super().register_compatible_head_model(lang_module,
+                                                      other_objective,
+                                                      objective_args_for_head_config,
+                                                      preloaded_module)
 
     def _loss_for_hidden_states(self,
                                 student_hidden: Tuple[torch.FloatTensor],
@@ -62,9 +82,6 @@ class Distillation(Objective, abc.ABC):
             "If adding loss of the hidden states, student and teacher must have embeddings of the same dimension."
 
         cosine_loss = CosineEmbeddingLoss(reduction="mean")
-
-        # student_hidden = torch.vstack(student_hidden)
-        # teacher_hidden = torch.vstack(teacher_hidden)
 
         if teacher_select_method == "alternate":
             teacher_student_ratio = len(teacher_hidden) / len(student_hidden)
@@ -83,14 +100,17 @@ class Distillation(Objective, abc.ABC):
         teacher_hidden_selected = torch.vstack([h.unsqueeze(0) for h in teacher_hidden_selected])
 
         if self.restrict_loss_to_mask:
+            # compute loss only from the attended positions
             attn_mask_reshaped = attn_mask.unsqueeze(-1).unsqueeze(0).expand_as(student_hidden).bool()
 
             student_hidden_flat = torch.masked_select(student_hidden, attn_mask_reshaped)
             teacher_hidden_selected_flat = torch.masked_select(teacher_hidden_selected, attn_mask_reshaped)
 
+            # we flatten the batch, to get the class scores & probabilities to the 2nd dimension
             student_hidden_unbatched = student_hidden_flat.reshape(-1, student_hidden.shape[-1])
             teacher_hidden_unbatched = teacher_hidden_selected_flat.reshape(-1, student_hidden.shape[-1])
         else:
+            # we flatten the batch, to get the class scores & probabilities to the 2nd dimension
             student_hidden_unbatched = student_hidden.reshape(-1, student_hidden.shape[-1])
             teacher_hidden_unbatched = teacher_hidden_selected.reshape(-1, student_hidden.shape[-1])
 
@@ -103,6 +123,7 @@ class Distillation(Objective, abc.ABC):
                             teacher_outputs: ModelOutput,
                             attn_mask: torch.LongTensor) -> torch.FloatTensor:
         if hasattr(teacher_outputs, "hidden_states"):
+            # encoder-, or decoder-only
             assert hasattr(student_outputs, "hidden_states"), "Student and teacher must be of the same type"
             student_hidden = student_outputs.hidden_states
             teacher_hidden = teacher_outputs.hidden_states
@@ -110,6 +131,7 @@ class Distillation(Objective, abc.ABC):
             loss = self._loss_for_hidden_states(student_hidden, teacher_hidden, attn_mask)
 
         elif hasattr(teacher_outputs, "encoder_hidden_states") and hasattr(teacher_outputs, "decoder_hidden_states"):
+            # encoder-decoder: sum the losses for both encoder and decoder states
             assert hasattr(student_outputs, "encoder_hidden_states") \
                    and hasattr(teacher_outputs, "decoder_hidden_states"), "Student and teacher must be of the same type"
 
@@ -121,7 +143,7 @@ class Distillation(Objective, abc.ABC):
             loss = (self._loss_for_hidden_states(student_encoder_hidden, teacher_encoder_hidden, attn_mask) +
                     self._loss_for_hidden_states(student_decoder_hidden, teacher_decoder_hidden, attn_mask))
         else:
-            raise ValueError("Please initialize teacher model with `output_hidden_states=True`")
+            raise ValueError("Please initialize both teacher and student model with `output_hidden_states=True`")
 
         return loss
 
@@ -131,6 +153,7 @@ class Distillation(Objective, abc.ABC):
                       inputs: Optional[Union[BatchEncoding, Dict[str, torch.Tensor]]] = None) -> torch.FloatTensor:
         assert inputs is not None, "Distillation loss requires model inputs to be passed"
 
+        # output logits' loss
         ce_loss = CrossEntropyLoss()
 
         teacher_inputs = inspect.getfullargspec(self.teacher_model.forward).args
@@ -145,6 +168,7 @@ class Distillation(Objective, abc.ABC):
             student_logits_flat = torch.masked_select(student_logits, attn_mask_reshaped)
             student_logits_unbatched = student_logits_flat.reshape(-1, student_logits.shape[-1])
 
+            # we flatten the batch, to get the class scores & probabilities to the 2nd dimension
             teacher_logits_flat = torch.masked_select(teacher_logits, attn_mask_reshaped)
             teacher_logits_unbatched = teacher_logits_flat.reshape(-1, student_logits.shape[-1])
         else:
@@ -155,8 +179,10 @@ class Distillation(Objective, abc.ABC):
         distil_loss = ce_loss(log_softmax(student_logits_unbatched / self.temperature, dim=-1),
                               softmax(teacher_logits_unbatched / self.temperature, dim=-1)) * self.temperature ** 2
         distil_loss = self.logits_ce_loss_weight * distil_loss
+        # end output logits' loss
 
         if self.add_hidden_states_loss:
+            # hidden states loss
             student_inputs = inspect.getfullargspec(self.compatible_head_model.forward).args
             student_outputs = self.compatible_head_model(**{k: v for k, v in inputs.items() if k in student_inputs})
 
