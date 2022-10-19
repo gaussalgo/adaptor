bc6a941a3f4255ba1db0a95d85cd7e8bf0ec92aa
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 06:03:52 +0000

    Adding tarfile member sanitization to extractall()

--- adaptor/evaluators/prism.py
+++ adaptor/evaluators/prism.py
@@ -72,7 +72,26 @@ class Prism:
             print(f'Extracting Prism model to {model_path}')
 
             with tarfile.open(tarfile_path, 'r') as tar:
-                tar.extractall(path=model_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, path=model_path)
             tarfile_path.unlink()
 
         extraction_folder = DEFAULT_MODEL_URL.split("/")[-1].split(".")[0]
