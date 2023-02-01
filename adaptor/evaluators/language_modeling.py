441247e37a19e84de6d336db169b6c54b90b9dc1
stefanik12 <stefanik.mich@gmail.com>
2023-02-01 22:26:58 +0100

    Train_with_sampler, debugger

--- adaptor/evaluators/language_modeling.py
+++ adaptor/evaluators/language_modeling.py
@@ -1,4 +1,5 @@
-from typing import List
+import abc
+from typing import List, Tuple
 
 import torch
 from transformers import PreTrainedTokenizer
@@ -9,7 +10,26 @@ from ..utils import Head, AdaptationDataset
 
 class Perplexity(EvaluatorBase):
 
-    compatible_heads: List[Head] = [Head.MLM, Head.CLM, Head.SEQ2SEQ]
+    compatible_heads: List[Head] = [Head.MLM, Head.CLM]
+
+    @staticmethod
+    def _per_batch_perplexity(model: torch.nn.Module,
+                              tokenizer: PreTrainedTokenizer,
+                              dataset: AdaptationDataset) -> Tuple[List[int], List[int]]:
+        pass
 
     def __call__(self, model: torch.nn.Module, tokenizer: PreTrainedTokenizer, dataset: AdaptationDataset) -> float:
-        raise NotImplementedError()
+
+        per_batch_perplexity = 0
+        num_samples = 0
+
+        for batch in dataset:
+            masks_pos = batch["input_ids"] == tokenizer.mask_token_id
+            expected_tokens = batch["labels"][masks_pos]
+            actual_tokens = model(**batch).logits.argmax(-1).flatten().tolist()
+
+            expected.extend(expected_tokens)
+            actual.extend(actual_tokens)
+
+        return expected, actual
+
