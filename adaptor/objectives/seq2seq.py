193b347f6fcfb1a7e4c831230a89e20b4bf7d8bc
stefanik12 <stefanik.mich@gmail.com>
2024-04-03 13:52:09 +0300

    Remove constraints on seq2seq objective & propagation of kwargs to evaluators generate()

--- adaptor/objectives/seq2seq.py
+++ adaptor/objectives/seq2seq.py
@@ -106,22 +106,6 @@ class Sequence2SequenceMixin(SequentialMixin, abc.ABC):
 
         return lm_loss
 
-    def register_compatible_head_model(self,
-                                       lang_module: LangModule,
-                                       other_objective: Optional["Objective"] = None,
-                                       objective_args_for_head_config: Optional[Dict[str, Any]] = None,
-                                       preloaded_module: Optional[torch.nn.Module] = None) -> torch.nn.Module:
-
-        head_module = super().register_compatible_head_model(lang_module, other_objective,
-                                                             objective_args_for_head_config, preloaded_module)
-        assert hasattr(head_module, "prepare_decoder_input_ids_from_labels"), \
-            "No head of the loaded LangModule is compatible with %s objective! " \
-            "\nNote that the module compatible with " \
-            "Sequence2SequenceMixin \nmust have `prepare_decoder_input_ids_from_labels` method, " \
-            "see e.g. \ntransformers.BartModel." % self
-
-        return head_module
-
 
 class Sequence2Sequence(Sequence2SequenceMixin, SupervisedObjective):
 
