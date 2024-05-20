ef34f7fa15e354b9ca051411f5e5e564fcd887f3
stefanik12 <stefanik.mich@gmail.com>
2024-05-20 14:02:56 +0200

    continued training test use PEFT objectives to save space

--- tests/adapter_test.py
+++ tests/adapter_test.py
@@ -14,6 +14,18 @@ from utils import paths, test_base_models
 SAVE_STEPS = 1
 
 
+training_args_map = {
+    "output_dir": "adaptation_output_dir",
+    "stopping_strategy": StoppingStrategy.FIRST_OBJECTIVE_NUM_EPOCHS,
+    "do_train": True,
+    "do_eval": True,
+    "gradient_accumulation_steps": 2,
+    "log_level": "critical",
+    "logging_steps": 1,
+    "num_train_epochs": 3
+}
+
+
 training_arguments = AdaptationArguments(output_dir="adaptation_output_dir",
                                          stopping_strategy=StoppingStrategy.FIRST_OBJECTIVE_NUM_EPOCHS,
                                          do_train=True,
@@ -42,9 +54,10 @@ def test_ner_adaptation():
                                       labels_or_path=paths["labels"]["ner"],
                                       batch_size=1)]
 
-    schedule = SequentialSchedule(objectives, training_arguments)
+    args = AdaptationArguments(**training_args_map)
 
-    adapter = Adapter(lang_module, schedule, args=training_arguments)
+    schedule = SequentialSchedule(objectives, args)
+    adapter = Adapter(lang_module, schedule, args=args)
 
     run_adaptation(adapter)
 
@@ -59,9 +72,10 @@ def test_mt_adaptation():
                                     labels_or_path=paths["labels"]["translation"],
                                     batch_size=1)]
 
-    schedule = SequentialSchedule(objectives, training_arguments)
+    args = AdaptationArguments(**training_args_map)
 
-    adapter = Adapter(lang_module, schedule, args=training_arguments)
+    schedule = SequentialSchedule(objectives, args)
+    adapter = Adapter(lang_module, schedule, args=args)
 
     run_adaptation(adapter)
 
@@ -78,9 +92,10 @@ def test_mt_adaptation_bt():
                                     labels_or_path=paths["labels"]["translation"],
                                     batch_size=1)]
 
-    schedule = SequentialSchedule(objectives, training_arguments)
+    args = AdaptationArguments(**training_args_map)
 
-    adapter = Adapter(lang_module, schedule, args=training_arguments)
+    schedule = SequentialSchedule(objectives, args)
+    adapter = Adapter(lang_module, schedule, args=args)
 
     run_adaptation(adapter)
 
@@ -91,15 +106,18 @@ def test_continued_training():
     objectives = [BackTranslation(lang_module,
                                   back_translator=translator,
                                   texts_or_path=paths["texts"]["unsup"],
-                                  batch_size=2),
+                                  batch_size=2,
+                                  peft_objective=True),
                   Sequence2Sequence(lang_module,
                                     texts_or_path=paths["texts"]["translation"],
                                     labels_or_path=paths["labels"]["translation"],
-                                    batch_size=1)]
+                                    batch_size=1,
+                                    peft_objective=True)]
 
-    schedule = ParallelSchedule(objectives, training_arguments)
+    args = AdaptationArguments(**{**training_args_map, **{"eval_steps": SAVE_STEPS, "save_steps": SAVE_STEPS}})
 
-    adapter = Adapter(lang_module, schedule, args=training_arguments)
+    schedule = SequentialSchedule(objectives, args)
+    adapter = Adapter(lang_module, schedule, args=args)
 
     # first training iteration
     run_adaptation(adapter)
